8b11ce02ec422c7eea0749d28a2f2e1a79b7a206
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2017-03-29 00:52:03 +0200

    Added example of calculation chi-squared in R

---
+++ syllabus/data/crosstab.R
@@ -0,0 +1,42 @@
+# Contingency table example: appreciation of products between
+# women and men.
+# 
+# Source: http://www.cyclismo.org/tutorial/R/tables.html#creating-a-table-directly
+
+# Construct the contingency table. This would normally be calculated 
+# from the frequencies of ordinal/nominal variables, e.g.
+# table(observations$appreciation, observations$gender)
+
+appreciation_m <- matrix(c(9,8,5,0,8,10,5,4), ncol = 2)
+rownames(appreciation_m) <- 
+  c("Good", "Sufficient", "Insufficient", "Bad")
+colnames(appreciation_m) <- c("Women", "Men")
+appreciations <- as.table(appreciation_m)
+
+# Calculate marginal totals:
+margin.table(appreciations, 1)  # Row totals
+margin.table(appreciations, 2)  # Column totals
+margin.table(appreciations)     # General total (# observatios)
+
+# Percentages w.r.t. row totals
+prop.table(appreciations, 2)
+
+# Calculation of chi-squared, the hard way
+# Expected values (calculated with matrix product)
+expected <- as.array(margin.table(appreciations,1)) %*%
+  t(as.array(margin.table(appreciations,2))) / 
+  margin.table(appreciations)
+# Squared differences, normalised
+differences <- (appreciations - expected) ^ 2 / expected
+# Chi-square:
+sum(differences)
+
+# Rechtstreekse berekening chi-kwadraatwaarde
+summ <- summary(appreciations)
+chi_sq <- summ$statistic
+
+# Cramér's V
+k <- min(nrow(appreciations), ncol(appreciations))
+V <- sqrt(chi_sq/
+            (margin.table(appreciations) *
+               (k - 1)))
